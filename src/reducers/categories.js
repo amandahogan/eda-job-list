13a5e333558c4a3ea7a0074cf7c81ec92be324c8
David Christie <davidchristie.nz@gmail.com>
2017-03-03 14:46:09 +1300

    Add colour property to categories

--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -1,13 +1,16 @@
 const initialState = [
   {
+    colour: 'violet',
     id: 1,
     name: 'Languages'
   },
   {
+    colour: 'green',
     id: 2,
     name: 'Locations'
   },
   {
+    colour: 'blue',
     id: 3,
     name: 'Technologies'
   }
