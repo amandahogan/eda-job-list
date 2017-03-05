88d2328595b073ef2ec92c25b0b3755501124b6c
David Christie <davidchristie.nz@gmail.com>
2017-03-06 10:59:05 +1300

    Close #30

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -7,12 +7,18 @@ import './Details.css'
 class Details extends React.Component {
   render () {
     return (
-      <Modal open={this.props.open} onClose={this.props.close} >
-        <Modal.Header>Profile Picture</Modal.Header>
+      <Modal
+        closeIcon='close'
+        onClose={this.props.close}
+        open={this.props.open}
+      >
+        <Modal.Header>{this.props.company.name}</Modal.Header>
         <Modal.Content image>
           <Image wrapped size='medium' src='http://semantic-ui.com/images/wireframe/image.png' />
           <Modal.Description>
-            <Header>Tags</Header>
+            <Header>Description:</Header>
+            {this.props.company.description}
+            <Header>Tags:</Header>
             {this.props.categories.map((category, index) => {
               return <Category
                 category={category}
@@ -22,11 +28,6 @@ class Details extends React.Component {
             })}
           </Modal.Description>
         </Modal.Content>
-        <Modal.Actions>
-          <Button primary>
-            Proceed <Icon name='right chevron' />
-          </Button>
-        </Modal.Actions>
       </Modal>
     )
   }
